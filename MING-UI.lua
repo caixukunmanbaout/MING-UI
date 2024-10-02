87e59a216e4c3b75fc02e98749a63832606dfd29
SYF-Hub <2845964845@qq.com>
2024-10-02 23:03:09 +0800

    Update MING-UI.lua

--- MING-UI.lua
+++ MING-UI.lua
@@ -15,9 +15,9 @@ local OrionLib = {
 			Main = Color3.fromRGB(355, 255, 325),
 			Second = Color3.fromRGB(92, 62, 62),
 			Stroke = Color3.fromRGB(65, 35, 65),
-			Divider = Color3.fromRGB(120, 160, 349),
-			Text = Color3.fromRGB(130, 160, 236),
-			TextDark = Color3.fromRGB(130, 160, 236)
+			Divider = Color3.fromRGB(1, 160, 349),
+			Text = Color3.fromRGB(1, 160, 236),
+			TextDark = Color3.fromRGB(1, 160, 236)
 		}
 	},
 	SelectedTheme = "Default",
