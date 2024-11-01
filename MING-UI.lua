c1b5dbd76ef4f75193cafbccfed78cd04ba0dc59
傻逼 <2845964845@qq.com>
2024-11-02 01:00:18 +0800

    Update MING-UI.lua

--- MING-UI.lua
+++ MING-UI.lua
@@ -561,7 +561,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				Font = Enum.Font.FredokaOne,
 				ClipsDescendants = true
 			}), "Text"),
-			AddThemeObject(SetProps(MakeElement("Label", LocalPlayer.DisplayName, WindowConfig.HidePremium and 14 or 13), {
+AddThemeObject(SetProps(MakeElement("Label", WindowConfig.Name, 14), {
 				Size = UDim2.new(1, -60, 0, 12),
 				Position = UDim2.new(0, 50, 1, -25),
 				Visible = not WindowConfig.HidePremium
